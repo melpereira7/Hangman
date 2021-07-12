93bb7b3ce3ff532cdf55a39122a16d6d4123444a
melpereira7 <43989969+melpereira7@users.noreply.github.com>
2021-07-12 01:05:18 +0100

    Delete Topics directory

--- Topics/Elif statement/Index of synthesis/main.py
+++
@@ -1,8 +0,0 @@
-index = float(input())
-
-if index < 2:
-    print('Analytic ')
-elif 2 <= index <= 3:
-    print('Synthetic ')
-elif index > 3:
-    print('Polysynthetic ')
--- Topics/Search in a string/Simon says/main.py
+++
@@ -1,7 +0,0 @@
-def what_to_do(instructions):
-    if instructions.startswith('Simon says'):
-        return f"I {instructions.replace('Simon says ','')}"
-    elif instructions.endswith('Simon says'):
-        return f"I {instructions.replace('Simon says', '')}"
-    else:
-        return "I won't do it!"
